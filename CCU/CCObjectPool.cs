d6249003416be78c9ccddc05542c95e88761cb7a
CC-dreamWork <1041985437@qq.com>
2018-03-26 09:22:14 +0800

    aaa

---
+++ CCU/CCObjectPool.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+
+namespace CCU
+{
+    public static class CCObjectPool
+    {
+        //private static CCObjectPool _ins;
+
+        //public static CCObjectPool Ins
+        //{
+        //    get
+        //    {
+        //        if (_ins == null) _ins = new CCObjectPool();
+        //        return _ins;
+        //    }
+        //}
+
+        /// <summary>
+        /// 取出
+        /// </summary>
+        public static T TakeOut<T>(string url) where T:Object
+        {
+            T obj = LoadManager.Ins.Load<T>(url);
+            return obj;
+        }
+        /// <summary>
+        /// 存储
+        /// </summary>
+        public static bool Storage<T>(string name)
+        {
+            return false;
+        }
+
+
+    }
+
+}
---
+++ CCU/LoadManager.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace CCU
+{
+    public class LoadManager
+    {
+        private static LoadManager _ins;
+
+        public static LoadManager Ins
+        {
+            get
+            {
+                if(_ins==null) _ins=new LoadManager();
+                return _ins;
+            }
+        }
+
+        public T Load<T>(string url) where T : Object
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                Debug.LogError("LoadManage.LoadRes load failed , url = " + url);
+                return null;
+            }
+            T obj = Resources.Load<T>(url);
+            if (obj == null)
+            {
+                Debug.LogError("LoadManage.LoadRes load failed , url = " + url);
+            }
+            return obj;
+        }
+    }
+
+}
