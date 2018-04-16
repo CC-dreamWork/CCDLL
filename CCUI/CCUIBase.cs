785ee2f99cf24f0a6ca86f5afbe8106606ae31cb
CC-dreamWork <1041985437@qq.com>
2018-04-16 18:05:41 +0800

    aaa

---
+++ CCUI/CCUIBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CC.UI
+{
+   public class CCUIBase
+    {
+    }
+}
---
+++ CCUI/CCUIManager.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using CCU;
+
+namespace CC.UI
+{
+    public class CCUIManager
+    {
+        private static CCUIManager _ins;
+
+        public static CCUIManager Ins
+        {
+            get
+            {
+                if (_ins == null) _ins = new CCUIManager();
+                return _ins;
+            }
+        }
+
+        private readonly Dictionary<int, string> _uiUrl=new Dictionary<int, string>();
+        private readonly Dictionary<int, GameObject> _uiParams=new Dictionary<int, GameObject>();
+        public CCUIManager() {
+            _uiUrl.Add((int)CCUiType.MainUi,"");
+        }
+
+        private void AddUi()
+        {
+
+        }
+
+        private GameObject UiParms(CCUiType ccuitype)
+        {
+            int type = (int)ccuitype;
+            GameObject go;
+            if (_uiParams.ContainsKey(type)) go = _uiParams[type];
+            else  go=LoadManager.Ins.Load<GameObject>(_uiUrl[type]);
+            return go;
+        }
+
+        public void OpenUi(CCUiType ccuitype,bool coexist=false)
+        {
+            _uiParams[(int)ccuitype].transform.SetAsLastSibling();
+        }
+    }
+}
